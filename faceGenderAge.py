acb7e645d657e1948c25f700058f0622553fe5e9
William Tenório <william.tenorio@loopix.com.br>
2019-07-17 13:40:33 -0300

    ajustes

--- faceGenderAge.py
+++ faceGenderAge.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
 	age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
 	gender_list = ['Homem', 'Mulher']
 
-	#unziperFile( source, destination )
+	unziperFile( source, destination )
 	age_net, gender_net = initialize_caffe_models()
 	read_from_camera(age_net, gender_net, destination  )
 
